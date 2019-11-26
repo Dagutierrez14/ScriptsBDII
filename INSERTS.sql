92c5a066e1aad4ddeeba07e73b295dda3fa41c16
LuisRF54 <luisfuentes054@hotmail.com>
2019-11-25 21:07:08 -0400

    merge

--- INSERTS.sql
+++ INSERTS.sql
@@ -1,5 +1,21 @@
 CREATE OR REPLACE DIRECTORY FOTOS AS 'C:\Imagenes-Inserts';
 
+---------------------------------------------Triggers de Creacion----------------------------------------------
+CREATE OR REPLACE TRIGGER CREAR_CUENTA_MILLA
+AFTER INSERT ON USUARIO
+FOR EACH ROW
+    BEGIN
+       INSERT INTO CUENTA_MILLA (cantidad,usuario_fk) VALUES (200,:new.clave); 
+    END;
+/
+
+CREATE OR REPLACE TRIGGER CREAR_ESTATUS_HABITACION
+AFTER INSERT ON HABITACION
+FOR EACH ROW
+    BEGIN
+        INSERT INTO ESTATUS_HABITACION (fecha,habitacion_fk,estatus_fk) VALUES (to_date('2017-12-01 00:00:00','yyyy-mm-dd HH24:MI:SS'),:new.clave,6);
+    END;
+/
 ----------------------------------------Lugar-Paises-------------------------------------------------------------------------------
 /*1*/  INSERT INTO LUGAR (Nombre,Tipo) VALUES ('Venezuela','Pais'); 
 /*2*/  INSERT INTO LUGAR (Nombre,Tipo) VALUES ('USA','Pais');
